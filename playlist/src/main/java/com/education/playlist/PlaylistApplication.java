ccb155060fec9c6c0e08ea4e03832b883c7c5ffc
Puneet Kumar <puneet.kumar@walmart.com>
2020-11-18 15:23:38 -0800

    Spring Initializer

---
+++ playlist/src/main/java/com/education/playlist/PlaylistApplication.java
@@ -0,0 +1,14 @@
+package com.education.playlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@ComponentScan
+@SpringBootApplication
+public class PlaylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistApplication.class, args);
+	}
+
+}
