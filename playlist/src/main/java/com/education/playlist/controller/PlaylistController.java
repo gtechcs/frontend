2fb99613061b1ae42c47f1cf895b2e2b2fc19cd4
Puneet Kumar <puneet.kumar@walmart.com>
2020-11-18 18:18:05 -0800

    Add PlaylistController Service

--- playlist/src/main/java/com/education/playlist/controller/PlaylistController.java
+++ playlist/src/main/java/com/education/playlist/controller/PlaylistController.java
@@ -1,15 +1,21 @@
 package com.education.playlist.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.education.playlist.service.PlaylistService;
 
 @Controller
 public class PlaylistController {
 
-	@GetMapping("/")
-	public String listPlaylistAndSongs() {
+	@Autowired
+	private PlaylistService playlistService;
 
+	@GetMapping("/")
+	public String listPlaylistAndSongs(Model model) {
+		model.addAttribute("playlists", playlistService.getAllPlaylists());		
 		return "index";
 	}
 
