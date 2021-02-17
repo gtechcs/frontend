d43da549914d4dfbaec076d8447316943117144e
Puneet Kumar <puneet.kumar@walmart.com>
2021-02-17 11:59:33 -0800

    yacms

---
+++ yacms-nextjs/README.md
@@ -0,0 +1,5 @@
+This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+
+
+npx create-next-app yacms-nextjs --use-npm --example "https://github.com/vercel/next-learn-starter/tree/master/learn-starter"
+
---
+++ yacms-nextjs/pages/index.js
@@ -0,0 +1,36 @@
+import Head from 'next/head'
+import React from "react";
+import ReactDOM from "react-dom";
+import Page from "../src/client/page";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+      <p>Hello world!</p>
+      <Page />
+
+      </main>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
